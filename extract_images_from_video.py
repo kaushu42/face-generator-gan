8ca2a8a46075e9d658304f4ed07be7975910a8ec
Kaushal <kaushu42@gmail.com>
2019-08-11 20:11:47 +0545

    Added cascade

--- extract_images_from_video.py
+++ extract_images_from_video.py
@@ -1,38 +1,52 @@
 import os
 import cv2
 
-eye_classifier = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
+# Load the haar cascade to detect the faces
 face_classifier = cv2.CascadeClassifier(
     'cascades/haarcascade_frontalface_default.xml')
 
 
-def detect_face_and_eyes(image):
+def detect_faces(image):
+    '''
+            Detect the faces in the input image.
+            The image is converted to a grayscale image, then the cascade is applied.
+
+            Input: An input image
+            Returns: A generator that yields all the faces in the input image
+    '''
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(grey, 1.5, 5)
-    roi_original = None
     for (x, y, w, h) in faces:
-        roi_grey = grey[y:y+h, x:x+w]
-        roi_original = image[y:y+h, x:x+w]
-        yield roi_original
+        face = image[y:y+h, x:x+w]
+        yield face
 
-input_files = os.listdir('videos')
+
+# Read the video files from the videos directory
+VIDEO_DIR = 'videos'
+input_files = os.listdir(VIDEO_DIR)
 input_files.remove('help.txt')
-for input_file in input_files:
-	cap = cv2.VideoCapture(input_file)
-	count = 0
-	filecount = 0
-	while True:
-	    _, frame = cap.read()
-	    count += 1
-	    if count % 2 == 0:
-	        images = detect_face_and_eyes(frame)
-	        for image in images:
-	            resized = cv2.resize(image, (81, 81))
-	            cv2.imshow('Video', resized)
-	            # file = f'images/image-{filecount}.png'
-	            # cv2.imwrite(file, resized)
-	            # filecount += 1
 
+IMAGE_DIR = 'inputs'
 
-	cap.release()
-	cv2.destroyAllWindows()
+if not len(input_files):
+    print('No videos in video directory. Please add some files')
+    exit()
+for file_count, input_file in enumerate(input_files):
+    cap = cv2.VideoCapture(os.path.join(VIDEO_DIR, input_file))
+    frame_count = 0
+    print(input_file)
+    while True:
+        _, frame = cap.read()
+        if frame_count % 5 == 0:
+            images = detect_faces(frame)
+            for i, image in enumerate(images):
+                resized = cv2.resize(image, (81, 81))
+                file = os.path.join(
+                    IMAGE_DIR, f'{file_count}-{frame_count}-{i}.png')
+                cv2.imwrite(file, resized)
+        frame_count += 1
+        if frame is None:
+            print('END')
+            break
+    cap.release()
+    cv2.destroyAllWindows()
